e47f5cedebb1f22dc766185fbca135f366db6139
longfeizheng <longfei_zheng@163.com>
2018-04-09 20:06:02 +0800

    添加spring-boot-starter-actuator实现order配置自动更新（集成webhook）

--- config/readme.md
+++ config/readme.md
@@ -8,3 +8,7 @@
 
 
 basedir: ?
+
+自动刷新路径:curl -v -X POST "http://localhost:8082/application/refresh"
+
+集成webhook实现自动更新：https://natapp.cn
--- order/order-server/src/main/java/cn/merryyou/order/controller/EnvController.java
+++ order/order-server/src/main/java/cn/merryyou/order/controller/EnvController.java
@@ -1,6 +1,7 @@
 package cn.merryyou.order.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/env")
+@RefreshScope
 public class EnvController {
 
     @Value("${env}")
