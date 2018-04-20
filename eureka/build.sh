b377747e191007f11c0e166300661adcaa58d8ce
longfeizheng <longfei_zheng@163.com>
2018-04-20 20:15:26 +0800

    修改build.sh

--- eureka/build.sh
+++ eureka/build.sh
@@ -4,4 +4,4 @@ mvn clean package -Dmaven.test.skip=true -U
 
 docker build -t hub.c.163.com/longfeizheng/eureka .
 
-docker push hub.c.163.com/longfeizheng/eureka
+#docker push hub.c.163.com/longfeizheng/eureka
--- product/build.sh
+++ product/build.sh
@@ -4,4 +4,4 @@ mvn clean package -Dmaven.test.skip=true -U
 
 docker build -t hub.c.163.com/longfeizheng/product .
 
-docker push hub.c.163.com/longfeizheng/product
+#docker push hub.c.163.com/longfeizheng/product
